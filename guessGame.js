a5a8a4033267a99d3498fef4b0834787d328a34e
Muhammad Asif <159746725+Ali-Asif1@users.noreply.github.com>
2024-03-27 16:38:58 +0500

    number guessing game

---
+++ guessGame.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let generateNumber = Math.floor(Math.random() * 10 + 1);
+let userNumber = await inquirer.prompt([
+    { type: "number", name: "userNum", message: "Guess the number b/w 1 to 10:" },
+]);
+if (userNumber.userNum == generateNumber) {
+    console.log("your guess is right");
+}
+else {
+    console.log("Sorry, your guess is wrong");
+}
